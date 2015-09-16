9642ed6340d6e4600e46fdd53ba2be19cae5cbe9
Filip Ilievski <filip.dbrsk@gmail.com>
2015-09-16 16:28:40 +0200

    Store the resulting NTriples files (except for 120 incorrect files)

--- url_json_to_nt.py
+++ url_json_to_nt.py
@@ -5,7 +5,10 @@ import os, json
 
 def parse_ntriples(name):
 
-    q="https://raw.githubusercontent.com/barnaclebarnes/glamazon/master/organisations/" + name[0:3] + "/" + name
+
+    subpath=name[0:3] + "/" 
+    newpath="rdf/" + subpath + name.split(".")[0] + ".nt"
+    q="https://raw.githubusercontent.com/barnaclebarnes/glamazon/master/organisations/" + subfolder + name
     url = "http://rdf-translator.appspot.com/convert/json-ld/nt/" + q
     try:
         raw_result = urllib2.urlopen(url).read()
@@ -19,8 +22,6 @@ def parse_ntriples(name):
 
     g2=Graph()
 
-#    import pprint
-
     for t in g.triples((None, None, None)):
         if type(t[0])==rdflib.term.BNode:
             t0=rdflib.term.URIRef('http://lodlaundromat.org/.well-known/genid/' + t[0])
@@ -33,10 +34,12 @@ def parse_ntriples(name):
 
         g2.add((t0, t[1], t2))
 
-path="/Users/filipilievski/phd/glamazon/"
+    g2.serialize(destination=newpath, format='ntriples'))
+
+
+path="/scratch/fii800/glamazon/"
 
 w_err=open("log_errors.txt", "w")
-w_lic=open("log_lic.txt", "w")
 
 if __name__=="__main__":
     
